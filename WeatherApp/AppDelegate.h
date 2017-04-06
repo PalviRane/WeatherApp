3f40cc7e8e408568917fcf1465ab0389e3afe338
Potter <Palvi@Aakashs-MacBook-Air.local>
2017-04-07 00:49:23 +0530

    first commit

---
+++ WeatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherApp
+//
+//  Created by Potter on 07/04/17.
+//  Copyright © 2017 PalviRane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WeatherApp
+//
+//  Created by Potter on 07/04/17.
+//  Copyright © 2017 PalviRane. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
