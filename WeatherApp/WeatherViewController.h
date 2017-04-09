f0bc301d1d0ef79e47e1f7c7e5ec1d5448249789
Potter <Palvi@aakashs-macbook-air.local>
2017-04-09 19:42:01 +0530

    Weather place commit

---
+++ WeatherApp/WeatherViewController.h
@@ -0,0 +1,13 @@
+//
+//  WeatherViewController.h
+//  WeatherApp
+//
+//  Created by Potter on 09/04/17.
+//  Copyright © 2017 PalviRane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeatherViewController : UIViewController
+
+@end
---
+++ WeatherApp/WeatherViewController.m
@@ -0,0 +1,38 @@
+//
+//  WeatherViewController.m
+//  WeatherApp
+//
+//  Created by Potter on 09/04/17.
+//  Copyright © 2017 PalviRane. All rights reserved.
+//
+
+#import "WeatherViewController.h"
+
+@interface WeatherViewController ()
+
+@end
+
+@implementation WeatherViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+   // int randNum = rand() % ([max intValue] - [min intValue]+1) + [min intValue];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
